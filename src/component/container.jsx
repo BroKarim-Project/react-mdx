6a43835ad4ac4d61d5ebe68796bce3eb0f01ad1e
kiramDZ <bukutulis0110@gmail.com>
2024-07-27 17:25:56 +0700

    :lipstick: adding page transition with gsap

--- src/component/container.jsx
+++ src/component/container.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { MDXProvider } from "@mdx-js/react";
+import Template from "./template";
 
 //versi rapi nengok ccrsxx aja atau punya bawaaann boleh juga
 export const Container = ({ children, meta }) => {
@@ -11,26 +12,28 @@ export const Container = ({ children, meta }) => {
   ));
   return (
     <>
-      <div className="container min-h-screen mx-auto px-20 py-4">
-        <Link to={"/"} className="text-base font-medium py-2  px-4 rounded-md hover:bg-slate-200 hover:text-black dark:hover:bg-zinc-600">
-          &larr; Back
-        </Link>
-        <main className="pt-8 pb-2  text-black">
-          <div className="w-full ">
-            <img src={meta.banner} alt="" className="rounded-md mx-auto cursor-pointer hover:brightness-75 w-full  max-h-[448px] object-cover" />
-          </div>
-          <section className=" px-8 mt-8 grid gap-2">
-            <h1 className="text-2xl font-serif font-bold md:text-7xl">{meta.title}</h1>
-            <p className="text-lg opacity-80">Written on {meta.publishedAt} by Kariim</p>
-            <p className="text-lg opacity-80">{meta.description}</p>
-            <div className="tags-container flex gap-2">{tags}</div>
-            <hr className="mt-4 dark:border-gray-600 border-2" />
-            <section className="mt-4 prose max-w-4xl ">
-              <MDXProvider>{children}</MDXProvider>
+      <Template>
+        <div className="container min-h-screen mx-auto px-20 py-4">
+          <Link to={"/"} className="text-base font-medium py-2  px-4 rounded-md hover:bg-slate-200 hover:text-black dark:hover:bg-zinc-600">
+            &larr; Back
+          </Link>
+          <main className="pt-8 pb-2  text-black">
+            <div className="w-full ">
+              <img src={meta.banner} alt="" className="rounded-md mx-auto cursor-pointer hover:brightness-75 w-full  max-h-[448px] object-cover" />
+            </div>
+            <section className=" px-8 mt-8 grid gap-2">
+              <h1 className="text-2xl font-serif font-bold md:text-7xl">{meta.title}</h1>
+              <p className="text-lg opacity-80">Written on {meta.publishedAt} by Kariim</p>
+              <p className="text-lg opacity-80">{meta.description}</p>
+              <div className="tags-container flex gap-2">{tags}</div>
+              <hr className="mt-4 dark:border-gray-600 border-2" />
+              <section className="mt-4 prose max-w-4xl ">
+                <MDXProvider>{children}</MDXProvider>
+              </section>
             </section>
-          </section>
-        </main>
-      </div>
+          </main>
+        </div>
+      </Template>
     </>
   );
 };
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,52 +1,49 @@
-import React, { Suspense } from "react";
 import { a } from "@react-spring/web";
 import { Slider } from "../component/slider";
 import { items } from "../data/item";
 import Header from "../component/header";
-import Placeholder from "/placeholder.png";
 import { Link } from "react-router-dom";
+import Template from "../component/template";
+import MainNav from "../component/main-nav";
 
 import styles from "../styles/style.module.css";
 
 export default function Hero() {
-  const Earth = React.lazy(() => import("../component/earth/index"));
-
   return (
     <>
-      <div className="w-full px-20 h-screen  flex py-20 justify-center bg-[#faf7f1] relative ">
-        <div className={styles.main}>
-          <Header />
-          {/* <div className="flex items-center justify-center w-full h-[600px] absolute">
-            <Suspense fallback={<img src={Placeholder} className="h-[40vw] " alt="Loading..." />}>
-              <Earth />
-            </Suspense>
-          </div> */}
-          <div className="font-bold text-gray-700 rounded-full absolute bg-red-600 flex items-center justify-center font-mono" style={{ height: "500px", width: "500px", fontSize: "170px" }}>
-            404
-          </div>
-          <Slider items={items} width={1000} height={400} visible={3}>
-            {({ css }, i) => (
-              <Link to={items[i].url}>
-                <div className={styles.content}>
-                  {/* marker tu nomor */}
-                  <a.div className={`${styles.image} group relative block bg-black`} style={{ backgroundImage: css }}>
-                    <div className="relative p-4 sm:p-6 lg:p-8">
-                      {/* tag */}
-                      <p className="text-xl font-medium uppercase  p-2 bg-black text-pink-500 inline-block">Developer</p>
-                      {/* title  */}
-                      <p className="text-6xl font-bold text-white ">{items[i].title}</p>
-                      {/* desc */}
-                      <div className="mt-32 sm:mt-48 lg:mt-64">
-                        <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">{items[i].desc}</div>
+      <Template>
+        <MainNav />
+        <div className="w-full px-20 h-screen  flex py-20 justify-center bg-[#faf7f1] relative ">
+          <div className={styles.main}>
+            <Header />
+           
+            <div className="font-bold text-gray-700 rounded-full absolute bg-red-600 flex items-center justify-center font-mono" style={{ height: "500px", width: "500px", fontSize: "170px" }}>
+              404
+            </div>
+            <Slider items={items} width={1000} height={400} visible={3}>
+              {({ css }, i) => (
+                <Link to={items[i].url}>
+                  <div className={styles.content}>
+                    {/* marker tu nomor */}
+                    <a.div className={`${styles.image} group relative block bg-black`} style={{ backgroundImage: css }}>
+                      <div className="relative p-4 sm:p-6 lg:p-8">
+                        {/* tag */}
+                        <p className="text-xl font-medium uppercase  p-2 bg-black text-pink-500 inline-block">Developer</p>
+                        {/* title  */}
+                        <p className="text-6xl font-bold text-white ">{items[i].title}</p>
+                        {/* desc */}
+                        <div className="mt-32 sm:mt-48 lg:mt-64">
+                          <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">{items[i].desc}</div>
+                        </div>
                       </div>
-                    </div>
-                  </a.div>
-                </div>
-              </Link>
-            )}
-          </Slider>
+                    </a.div>
+                  </div>
+                </Link>
+              )}
+            </Slider>
+          </div>
         </div>
-      </div>
+      </Template>
     </>
   );
 }
